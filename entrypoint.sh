65fdb1711a7e06bdedef5e3b7a8d73b29a8a4742
Waleed Hammam <waleed.hamam@weave.works>
2023-03-09 14:37:18 +0200

    create flux namespace

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,8 @@ cat /tmp/key-config | gpg --batch --full-generate-key
 
 export KEY_FP=$(gpg --list-secret-keys "${KEY_NAME}" | sed -n "2 p" |  sed 's/^ *//g')
 
+kubectl create namespace flux-system --kubeconfig=/etc/gitops/value
+
 gpg --export-secret-keys --armor "${KEY_FP}" | \
 kubectl create secret generic sops-gpg \
 --namespace=flux-system \
