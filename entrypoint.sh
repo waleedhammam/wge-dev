7a61c18c885c8d7c20239360e87bcfff4be47bb0
Waleed Hammam <waleed.hamam@weave.works>
2023-03-14 00:10:25 +0200

    changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export KUBECONFIG=/etc/gitops/value
-export CLUSTER_NAME="temp"
 
 echo "=> generating keys"
 echo """%no-protection
@@ -12,9 +11,7 @@ Expire-Date: 0
 Name-Comment: ${KEY_COMMENT}
 Name-Real: ${KEY_NAME}
 """ > /tmp/key-config
-
 cat /tmp/key-config | gpg --batch --full-generate-key
-
 export KEY_FP=$(gpg --list-secret-keys "${KEY_NAME}" | sed -n "2 p" |  sed 's/^ *//g')
 echo "=> generate success"
 
@@ -22,17 +19,17 @@ echo "=> creating secret on the cluster"
 gpg --export-secret-keys --armor "${KEY_FP}" | \
 kubectl create secret generic sops-gpg \
 --namespace=flux-system \
---from-file=sops.asc=/dev/stdin \
-
+--from-file=sops.asc=/dev/stdin
 echo "=> creating success"
 
 echo "=> deleting secret key"
 gpg --batch --yes --delete-secret-keys  "${KEY_FP}"
 echo "=> deleted"
 
-kubectl get kustomization my-secrets -n flux-system -o jsonpath="{.spec.path}" > /tmp/path.url
-
-git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}.git && cd ${GITHUB_REPO}/`cat /tmp/path.url`
+echo "=> pushing config and pub key to the repo"
+export KUSTOMIZATION_NAME=$(kubectl get kustomization my-secrets -n flux-system -o jsonpath="{.metadata.name}")
+export KUSTOMIZATION_PATH=$(kubectl get kustomization my-secrets -n flux-system -o jsonpath="{.spec.path}")
+git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}.git && cd ${GITHUB_REPO}/${KUSTOMIZATION_PATH}
 
 cat <<EOF > ./.sops.yaml
 creation_rules:
@@ -40,15 +37,13 @@ creation_rules:
     encrypted_regex: ^(data|stringData)$
     pgp: ${KEY_FP}
 EOF
-
 gpg --export --armor "${KEY_FP}" > ./.sops.pub.asc
 
-rm /tmp/path.url
-
-git checkout -b cluster-${CLUSTER_NAME}
+git checkout -b sops-${KUSTOMIZATION_NAME}
 git add .
 git commit -m "add public key and sops configuration" --quiet
 git push --set-upstream origin cluster-temp
 git push --quiet
+echo "=> pushed"
 
 echo "=> Setup complete"
