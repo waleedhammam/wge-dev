d32ce8d3a23b1f5d98db2620b3f5a9ad53155a12
Waleed Hammam <waleed.hamam@weave.works>
2023-03-30 00:15:43 +0200

    add repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export KUBECONFIG=/etc/gitops/value
 
-echo "=> generating sops gpg keys"
+echo "=> generate sops gpg keys"
 echo """%no-protection
 Key-Type: 1
 Key-Length: 4096
@@ -13,32 +13,36 @@ Name-Real: ${KEY_NAME}
 """ > /tmp/key-config
 cat /tmp/key-config | gpg --batch --full-generate-key
 export KEY_FP=$(gpg --list-secret-keys "${KEY_NAME}" | sed -n "2 p" |  sed 's/^ *//g')
-echo "✅ generation success"
+echo "✅ sops keys generated successfuly"
 
-echo "=> creating secret on the cluster"
+echo "=> ensure sops secrets namespace: ${SOPS_SECRET_REF_NAMESPACE} is created"
+kubectl create namespace ${SOPS_SECRET_REF_NAMESPACE} --dry-run=client -o yaml | kubectl apply -f -
+echo "✅ namespace available"
+
+echo "=> create/configure sops private key secret on the cluster"
 gpg --export-secret-keys --armor "${KEY_FP}" | \
 kubectl create secret generic ${SOPS_SECRET_REF} \
 --namespace=${SOPS_SECRET_REF_NAMESPACE} \
---from-file=sops.asc=/dev/stdin
+--from-file=sops.asc=/dev/stdin \
+--dry-run=client -o yaml | kubectl apply -f -
 echo "✅ creation success"
 
-echo "=> deleting secret key"
+echo "=> delete sops secret key"
 gpg --batch --yes --delete-secret-keys  "${KEY_FP}"
-echo "✅ private key is deleted"
+echo "✅ sops secret key is deleted"
 
-echo "=> creating secret for public key on the cluster"
+echo "=> create/configure sops public key on the cluster"
 gpg --export --armor "${KEY_FP}" | \
 kubectl create secret generic ${SOPS_SECRET_REF}-pub \
 --namespace=${SOPS_SECRET_REF_NAMESPACE} \
---from-file=sops.asc=/dev/stdin
+--from-file=sops.asc=/dev/stdin \
+--dry-run=client -o yaml | kubectl apply -f -
 echo "✅ creation success"
 
-echo "=> pushing config and pub key to the repo"
 
+echo "=> create and push rbac for public key secret"
 export CLUSTER_PATH="clusters/${CLUSTER_NAMESPACE}/${CLUSTER_NAME}"
 git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_USER}/${GITHUB_REPO}.git 
-
-echo "=> create rbac for public key secret"
 
 cat <<EOF > ${GITHUB_REPO}/${CLUSTER_PATH}/sops-${SOPS_SECRET_REF}-rbac.yaml
 apiVersion: rbac.authorization.k8s.io/v1
@@ -53,23 +57,21 @@ rules:
   verbs: ["list", "get"]
 EOF
 
-echo "✅ rbac creation success"
+git add -A
+git commit -m "add public key rbac and sops configuration" --quiet
+git pull --rebase && git push --quiet
+echo "✅ rbac created and pushed to git"
 
-cd ${GITHUB_REPO}/${CLUSTER_PATH} && mkdir sops && cd sops
-
-cat <<EOF > ./.sops.yaml
+if [[ $PUSH_TO_GIT == true ]]; then
+  echo "=> push sops creation rules and public key to git"
+  cd ${GITHUB_REPO}/${CLUSTER_PATH} && mkdir sops && cd sops
+  cat <<EOF > ./.sops.yaml
 creation_rules:
   - path_regex: .*.yaml
     encrypted_regex: ^(data|stringData)$
     pgp: ${KEY_FP}
 EOF
 
-git add -A
-git commit -m "add public key rbac and sops configuration" --quiet
-git pull --rebase && git push --quiet
-echo "✅ rbac pushed to git"
-
-if [[ $PUSH_TO_GIT == true ]]; then
   gpg --export --armor "${KEY_FP}" > ./.sops.pub.asc
   git checkout -b sops-${CLUSTER_NAME}
   git add .
@@ -81,6 +83,5 @@ if [[ $PUSH_TO_GIT == true ]]; then
 else
   echo "✖️ not pushing to public key to git"
 fi
-
 
 echo "✅ Setup complete"
